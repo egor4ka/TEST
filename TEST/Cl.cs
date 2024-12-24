c0b657186edaec44162a744d72bc73503da56c00
egor4ka <135698192+egor4ka@users.noreply.github.com>
2024-12-24 09:07:25 +0200

    Добавьте файлы проекта.

---
+++ TEST/Cl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+  
+    public class Cl
+    { 
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("НЕЛЬЗЯ ДЕЛИТЬ НА 0");
+            }
+
+            return a / b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using TEST;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            double a = 10;
+            double b = 5;
+            Cl adbf = new Cl();
+            Assert.Equal(2, adbf.Divide(a, b));
+        }
+        [Fact]
+
+        public void TestMethod2()
+        {
+            double a = 10;
+            double b = 0;
+            Cl adbf = new Cl();
+            Assert.Throws<DivideByZeroException>(()=>adbf.Divide(a,b));
+        }
+        [Theory]
+        [InlineData(6, 3, 2)]
+        [InlineData(12, 2, 6)]
+        public void TestMethod3(double a,double b ,double exp)
+        {
+            Cl adbf = new Cl();
+            Assert.Equal(exp, adbf.Divide(a, b));
+        }
+
+    }
+ }
